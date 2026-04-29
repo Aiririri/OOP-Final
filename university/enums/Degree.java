dde1a04eeb5b55fa9ce3da2fc87ca81e0cef6622
Almas1505 <100870489+Almas1505@users.noreply.github.com>
2026-04-29 15:39:44 +0500

    enums

---
+++ university/enums/Degree.java
@@ -0,0 +1,6 @@
+package enums;
+public enum Degree {
+    BACHELOR,
+    MASTER,
+    PHD
+}
---
+++ university/enums/School.java
@@ -0,0 +1,10 @@
+package enums;
+public enum School {
+    SITE,
+    BS,
+    SEOGI,
+    SCE,
+    KMA,
+    SAM,
+    ISE
+}
