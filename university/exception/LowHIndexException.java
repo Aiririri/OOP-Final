f86164579cf42d61ed380071e34ee0ad44eef5a8
Fomichyova Ekaterina <katjushaef@gmail.com>
2026-04-29 12:39:56 +0600

    Update LowHIndexException.java

--- university/exception/LowHIndexException.java
+++ university/exception/LowHIndexException.java
@@ -2,6 +2,6 @@ package university.exception;
 
 public class LowHIndexException extends Exception {
     public LowHIndexException(String name, int h) {
-        super("'" + name + "' cannot be supervisor: h-index=" + h + " (need >= 3).");
+        super("'" + name + "' cannot be supervisor: h-index=" + h + ".");
     }
 }
